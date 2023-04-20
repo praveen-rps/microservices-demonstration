9e9e6bc1db4f15a42a21883b523d20f476c1b60f
praveen-rps <109707042+praveen-rps@users.noreply.github.com>
2023-04-20 14:51:33 +0530

    Add files via upload

---
+++ gateway/src/main/java/com/example/microservices/gateway/GatewayConfigurations.java
@@ -0,0 +1,21 @@
+package com.example.microservices.gateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfigurations {
+	
+	
+	@Bean
+	public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+		return builder.routes()
+			.route(p->p.path("/notes/**").uri("lb://NOTES-SERVICE/notes"))
+			.route(p->p.path("/comments/**").uri("lb://COMMENT-SERVICE/comments"))
+			.build();
+		
+	}
+
+}
